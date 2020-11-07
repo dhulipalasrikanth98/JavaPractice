3a9b8ad6baebecd924ddca4f7472e367b03261c6
dhulipalasrikanth98 <dhulipalasrikanth98@outlook.com>
2020-11-07 18:57:10 +0530

    todays_changes

---
+++ JavaProgramming/src/com/srikanth/javaPractice/JavaVariables.java
@@ -0,0 +1,19 @@
+package com.srikanth.javaPractice;
+
+import java.util.Scanner;
+
+public class JavaVariables {
+  public static void main(String[] args) {
+	byte s=2;
+	int a =s;
+	System.out.println("the values are"+s+""+a);
+	//User Input
+	Scanner sc = new Scanner(System.out.toString());
+	int x = sc.nextInt();
+	String str = sc.nextLine();
+	String d =sc.nextLine();
+	System.out.println(str);
+	
+			
+}
+}
